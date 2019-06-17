7c38794f01547addb68b67213801593684e53ceb
Michael Farrell <michaeldavidfarrell@gmail.com>
2019-06-17 11:12:19 -0700

    burp

--- include/estimator_utils.h
+++ include/estimator_utils.h
@@ -27,3 +27,22 @@ Eigen::Matrix3d rotmItoB(const double phi, const double theta, const double psi)
 
   return rotm;
 }
+
+Eigen::Vector2d goalPixelMeasModel(double phi, double theta, double psi,
+                                   double pos_x, double pos_y, double rho)
+{
+  Vector3d pos_I;
+  pos_I(0) = pos_x;
+  pos_I(1) = pos_y;
+  pos_I(2) = 1. / rho;
+
+  Matrix3d Rot_I_b = rotmItoB(phi, theta, psi);
+
+  Vector3d pos_cam = q_b_c_.R() * Rot_I_b * pos_I;
+
+  double u_cam = fx_ * pos_cam(0) / pos_cam(2) + cx_;
+  double u_meas = goal_pix_meas(0);
+
+  double v_cam = fy_ * pos_cam(1) / pos_cam(2) + cy_;
+  double v_meas = goal_pix_meas(1);
+}
